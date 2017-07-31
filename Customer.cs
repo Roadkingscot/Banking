35d3419b5b1dc7e2df2c28ba0d3f5e0b9e6b1e53
Scot Haynes <roadkingscot@icloud.com>
2017-07-31 22:54:21 +0100

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Banking
+{
+    public class Customer : Person
+    {
+        public Customer(string firstName, string surname):base(firstName,surname)
+        {
+        }
+    }
+}
