9ba9d95f2935d6d1d9ba9e7ae09b630a95ec7cd9
Roadkingscot <roadkingscot@icloud.com>
2017-07-31 22:01:13 +0100

    Add files via upload

---
+++ IAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Banking;
+namespace Banking
+{
+	public interface IAccount
+	{
+		string AccountHolder { get; }
+		decimal Balance { get; }
+		void Deposit(decimal value);
+		void Withdraw(decimal value);
+		void Transfer(IAccount account, decimal value);
+		void AddAccountHolder(IPerson customer);
+	}
+}
---
+++ IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Banking
+{
+	public interface IPerson
+	{
+		string FirstName { get; set; }
+		string Surname { get; set; }
+		string Fullname { get; }
+		void AddAccount(IAccount account);
+		List<IAccount> Accounts();
+	}
+}
